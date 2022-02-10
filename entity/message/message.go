ebaca8360674e505ff2093bf58a5bb129308e0e8
Yan Toledo <yan.toledo@hotmail.com>
2022-02-10 19:21:54 -0300

    rename directory entities to entity

---
+++ entity/message/message.go
@@ -0,0 +1,25 @@
+package message
+
+import (
+	"errors"
+
+	"github.com/yantoledo/input-service/entity/customer"
+)
+
+type Message struct {
+	Text     string
+	Type     string
+	Customer customer.Customer
+}
+
+func NewMessage() *Message {
+	return &Message{}
+}
+
+func (m *Message) IsValid() error {
+	if m.Text == "" || m.Type == "" {
+		return errors.New("Invalid Message")
+	}
+
+	return nil
+}
