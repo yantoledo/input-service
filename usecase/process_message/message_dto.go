44a0e76b8630e29e078253e256a809ea720ea276
Yan Toledo <yan.toledo@hotmail.com>
2022-02-13 11:01:57 -0300

    adds process message usecase and tests

---
+++ usecase/process_message/message_dto.go
@@ -0,0 +1,17 @@
+package process_message
+
+import "github.com/yantoledo/input-service/entity/customer"
+
+type MessageDtoInput struct {
+	Text     string            `json:"text"`
+	Type     string            `json:"type"`
+	MediaUrl string            `json:"midia_url"`
+	Customer customer.Customer `json:"customer"`
+}
+
+type MessageDtoOutput struct {
+	Text     string            `json:"text"`
+	Type     string            `json:"type"`
+	MediaUrl string            `json:"midia_url"`
+	Customer customer.Customer `json:"customer"`
+}
---
+++ usecase/process_message/process_message_test.go
@@ -0,0 +1,40 @@
+package process_message
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/yantoledo/input-service/entity/customer"
+	service "github.com/yantoledo/input-service/infra/service/message_service"
+)
+
+func TestProcessMessageWhenItIsValid(t *testing.T) {
+	customer := customer.NewCustomer()
+	customer.Name = "John Lock"
+	customer.UniqueID = 1234
+	customer.UniqueClientID = 1234
+	customer.Source = 1
+
+	input := MessageDtoInput{
+		Text:     "Hi",
+		Type:     "Text",
+		MediaUrl: "https://www.url.com",
+		Customer: *customer,
+	}
+
+	expectedOutput := MessageDtoOutput{
+		Text:     "Hi",
+		Type:     "Text",
+		MediaUrl: "https://www.url.com",
+		Customer: *customer,
+	}
+
+	serviceMock := service.NewMessageServiceMock()
+
+	usecase := NewProcessMessage(serviceMock)
+	output, err := usecase.Execute(input)
+
+	assert.Nil(t, err)
+	assert.Equal(t, expectedOutput, output)
+
+}
