0ba776153af89fa576da0e1eb5b0c9cb78ac541b
Yan Toledo <yan.toledo@hotmail.com>
2022-02-18 14:22:35 -0300

    fix message service and mock

--- api/service/message_service/message_service.go
+++ api/service/message_service/message_service.go
@@ -1,22 +1,32 @@
 package message_service
 
 import (
+	"encoding/json"
+
 	"github.com/yantoledo/input-service/api/entity/message"
 	"github.com/yantoledo/input-service/infra/message_broker"
 )
 
 type MessageService struct {
-	PBClient message_broker.PublisherInterface
+	BrokerClient message_broker.BrokerInterface
 }
 
-func NewMessageService() *MessageService {
-	return &MessageService{}
+func NewMessageService(broker message_broker.BrokerInterface) *MessageService {
+	return &MessageService{BrokerClient: broker}
 }
 
 func (m *MessageService) Publish(message *message.Message) error {
-	url := "https://7a3f-186-193-220-142.ngrok.io" // TODO: We must get the url from a env variable in config file
+	body, err := json.Marshal(message)
+	if err != nil {
+		return err
+	}
 
-	err := m.PBClient.Publish(url, message)
+	brokerRequest := message_broker.BrokerRequest{
+		URL:  "https://7a3f-186-193-220-142.ngrok.io", // TODO: We must get the url from a env variable in config file
+		Body: body,
+	}
+
+	err = m.BrokerClient.Publish(brokerRequest)
 
 	if err != nil {
 		return err
--- api/service/message_service/mock.go
+++ api/service/message_service/mock.go
@@ -1,12 +1,16 @@
 package message_service
 
-import "github.com/yantoledo/input-service/api/entity/message"
+import (
+	"github.com/yantoledo/input-service/api/entity/message"
+	"github.com/yantoledo/input-service/infra/message_broker"
+)
 
 type MessageServiceMock struct {
+	BrokerClient message_broker.BrokerInterface
 }
 
-func NewMessageServiceMock() *MessageServiceMock {
-	return &MessageServiceMock{}
+func NewMessageServiceMock(brokerClient message_broker.BrokerInterface) *MessageServiceMock {
+	return &MessageServiceMock{BrokerClient: brokerClient}
 }
 
 func (c *MessageServiceMock) Publish(message *message.Message) error {
